3c7ebe7b3c2daea451e844ff48b5cda371186c8c
Yuning Zhang <3368002779@qq.com>
2026-09-22 00:06:16 +0800

    Delete build directory

--- build/yq_dart_aim/CMakeFiles/yq_dart_aim_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for yq_dart_aim_uninstall.
-# This may be replaced when dependencies are built.
--- build/yq_dart_aim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/dart_ws_on/src/yq_dart_aim
-# Build directory: /root/dart_ws_on/build/yq_dart_aim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
